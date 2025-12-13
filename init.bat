d2713b95b55bed3c70a18939c4110328b6faca1c
phaoyan <60375506+phaoyan@users.noreply.github.com>
2025-12-13 23:32:06 +0800

    同步

--- init.bat
+++ init.bat
@@ -8,8 +8,8 @@ if "%~1"=="" (
 )
 robocopy "%src%" "%dst%" /E /XD node_modules .git dist .vscode .idea .cache
 cd /d "%dst%"
+call npm ci
+call npm run tools setup -- "%1"
 git init
 git add .
 git commit -m "init"
-call npm ci
-call npm run tools setup -- "%1"
